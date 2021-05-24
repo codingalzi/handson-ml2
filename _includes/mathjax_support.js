d915bd599e012b80cc5ef83763b424601da46313
Gyesik Lee <gslee@hknu.ac.kr>
2021-05-24 18:30:22 +0900

    using js

---
+++ _includes/mathjax_support.js
@@ -0,0 +1,20 @@
+MathJax.Hub.Config({
+    TeX: {
+      equationNumbers: {
+        autoNumber: "AMS"
+      }
+    },
+    tex2jax: {
+    inlineMath: [ ['$', '$'] ],
+    displayMath: [ ['$$', '$$'] ],
+    processEscapes: true,
+  }
+});
+MathJax.Hub.Register.MessageHook("Math Processing Error",function (message) {
+          alert("Math Processing Error: "+message[1]);
+	      });
+MathJax.Hub.Register.MessageHook("TeX Jax - parse error",function (message) {
+	        alert("Math Processing Error: "+message[1]);
+	      });
+
+src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML">
